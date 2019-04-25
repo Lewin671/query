50c4d371d294c740a599fa6c5d40f43cad41e16d
Lewin671 <37481138+Lewin671@users.noreply.github.com>
2019-04-25 13:09:14 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,8 +4,8 @@ base_dir=$(cd "$(dirname "$0")"; pwd)
 
 echo "base_dir=\"$base_dir\"">>~/.bashrc
 
-echo "alias query=\"python $base_dir/main.py\"">> ~/.bashrc
+echo "alias query=\"python3 $base_dir/main.py\"">> ~/.bashrc
 
-python "$base_dir/init.py"
+python3 "$base_dir/init.py"
 
 echo "finished, please restart your terminal to enjoy it."
