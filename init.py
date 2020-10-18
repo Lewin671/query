ab36af0b3aad08992a6ee622381e3cc8e4bc242b
Lewin671 <2596736318@qq.com>
2020-10-18 23:52:01 +0800

    更改初始化的方式

--- init.py
+++ init.py
@@ -1,6 +1,29 @@
 # coding: utf-8
-from models import init_db
+from config import BASE_PATH, SOUND_DIR, LOGGER_URI, DATABASE_URI
+import os
 
 
-if __name__ == "__main__":
-    init_db.start_init()
+def init():
+    # 检测是否初始化
+    data_path = os.path.join(BASE_PATH, "data")
+
+    if not os.path.exists(data_path):
+        # 创建data文件夹
+        os.mkdir(data_path)
+
+    # 创建声音目录
+    if not os.path.exists(SOUND_DIR):
+        os.mkdir(SOUND_DIR)
+
+    # 创建日志文件
+    if not os.path.exists(LOGGER_URI):
+        file = open(LOGGER_URI, 'w')
+        file.close()
+
+    # 初始化数据库
+    if not os.path.exists(DATABASE_URI):
+        from models import init_db
+        init_db.start_init()
+
+
+init()
